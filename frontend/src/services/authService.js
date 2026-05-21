9737330d31069bea4d33d6f2d64720a7e3c17680
anastasija <anastasija.nechoska@student.um.si>
2026-05-21 12:01:12 +0200

    feat(auth): implement login and register pages

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8080/api/auth'
+
+export const register = async (firstName, lastName, email, password) => {
+  const response = await axios.post(`${API_URL}/register`, {
+    firstName,
+    lastName,
+    email,
+    password
+  })
+  return response.data
+}
+
+export const login = async (email, password) => {
+  const response = await axios.post(`${API_URL}/login`, {
+    email,
+    password
+  })
+
+  if (response.data.token) {
+    localStorage.setItem('token', response.data.token)
+  }
+
+  return response.data
+}
+
+export const logout = () => {
+  localStorage.removeItem('token')
+}
+
+export const getToken = () => {
+  return localStorage.getItem('token')
+}
+
+export const isLoggedIn = () => {
+  return !!localStorage.getItem('token')
+}
